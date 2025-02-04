79a4a3fada5da2eeef41e34c2f6a365bdc1d8782
woahGithub <137102772+woahGithub@users.noreply.github.com>
2025-02-04 13:38:39 +0900

    Update dump_fds_rom.bat

--- RSBJ01_FDS/dump_fds_rom.bat
+++ RSBJ01_FDS/dump_fds_rom.bat
@@ -1,15 +1,21 @@
 @echo off
 fds_rom.py %1
 
+if exist output_1.bin (
+    if not exist output_2.bin (
+        mgd1cnv output_1.bin %~n1.fds
+        del output_1.bin
+        exit /b
+    )
+)
+
 mgd1cnv output_1.bin 1st_a.bin
 mgd1cnv output_2.bin 1st_b.bin
 
 del output_1.bin
 del output_2.bin
 
-fds_rom_patch.py rom.fds 1st_a.bin 1st_b.bin
+fds_rom_patch.py %~n1.fds 1st_a.bin 1st_b.bin
 
 del 1st_a.bin
 del 1st_b.bin
-
-pause
